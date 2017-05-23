4aa13c5f55d37e5f270ba4bffc464d69d86bb0bb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-05-23 13:04:59 +0200

    fix on win

--- oscpack/osc/OscOutboundPacketStream.h
+++ oscpack/osc/OscOutboundPacketStream.h
@@ -47,23 +47,23 @@
 
 #if defined(__has_include)
   #if __has_include(<string_view>) &&  __cplusplus > 201402L
-    #define OSSIA_STRING_VIEW 1
+    #define OSCPACK_STRING_VIEW 1
     #include <string_view>
     namespace oscpack
     { using string_view = std::string_view; }
   #elif __has_include(<experimental/string_view>)
-    #define OSSIA_STRING_VIEW 1
+    #define OSCPACK_STRING_VIEW 1
     #include <experimental/string_view>
     namespace oscpack
     { using string_view = std::experimental::string_view; }
   #endif
 #endif
 
-#if !defined(OSSIA_STRING_VIEW)
+#if !defined(OSCPACK_STRING_VIEW)
   #if BOOST_VERSION >= 106100
     #define HAS_BOOST_STRING_VIEW
     #include <boost/utility/string_view.hpp>
-    namespace ossia
+    namespace oscpack
     { using string_view = boost::string_view; }
   #else
     #define HAS_BOOST_STRING_REF
